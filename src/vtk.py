021d4a03a967fbc9cb30568bad7a4439841ff044
Paulo A. Herrera <pauloa.herrera@gmail.com>
2010-11-29 08:58:47 +0100

    Small changes to README.txt

--- src/vtk.py
+++ src/vtk.py
@@ -144,6 +144,7 @@ class VtkGroup:
         self.xml.openElement("VTKFile")
         self.xml.addAttributes(type = "Collection", version = "0.1",  byte_order = _get_byte_order())
         self.xml.openElement("Collection")
+        self.root = os.path.dirname(filepath)
 
     def save(self):
         """ Closes this VtkGroup. """
@@ -151,19 +152,15 @@ class VtkGroup:
         self.xml.closeElement("VTKFile")
         self.xml.close()
     
-    def addFile(self, filepath, sim_time, root = None):
+    def addFile(self, filepath, sim_time):
         """ Adds file to this VTK group.
 
             PARAMETERS:
                 filepath: full path to VTK file.
                 sim_time: simulated time.
-                root: filename is written relative to this directory
         """
         # TODO: Check what the other attributes are for.
-        if root == None:
-            filename = os.path.relpath(filepath)
-        else:
-            filename = os.path.relpath(filepath, start = root)
+        filename = os.path.relpath(filepath, start = self.root)
         self.xml.openElement("DataSet")
         self.xml.addAttributes(timestep = sim_time, group = "", part = "0", file = filename)
         self.xml.closeElement()
