8a6f273e5594bec34a701b8a5de5aee8ad4de34b
Paulo A. Herrera <pauloa.herrera@gmail.com>
2010-12-01 14:13:13 +0100

    License changed to FreeBSD license.

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
-# *************************************************************************
-# * Copyright 2010 Paulo Herrera                                          *
-# *                                                                       *
-# * This file is part of EVTK.                                            *
-# *                                                                       *
-# * EVTK is free software: you can redistribute it and/or modify          *
-# * it under the terms of the GNU General Public License as published by  *
-# * the Free Software Foundation, either version 3 of the License, or     *
-# * (at your option) any later version.                                   *
-# *                                                                       *
-# * EVTK is distributed in the hope that it will be useful,               *
-# * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# * GNU General Public License for more details.                          *
-# *                                                                       *
-# * You should have received a copy of the GNU General Public License     *
-# * along with EVTK.  If not, see <http://www.gnu.org/licenses/>.         *
-# *************************************************************************
+# ***********************************************************************************
+# * Copyright 2010 Paulo A. Herrera. All rights reserved.                           * 
+# *                                                                                 *
+# * Redistribution and use in source and binary forms, with or without              *
+# * modification, are permitted provided that the following conditions are met:     *
+# *                                                                                 *
+# *  1. Redistributions of source code must retain the above copyright notice,      *
+# *  this list of conditions and the following disclaimer.                          *
+# *                                                                                 *
+# *  2. Redistributions in binary form must reproduce the above copyright notice,   *
+# *  this list of conditions and the following disclaimer in the documentation      *
+# *  and/or other materials provided with the distribution.                         *
+# *                                                                                 *
+# * THIS SOFTWARE IS PROVIDED BY PAULO A. HERRERA ``AS IS'' AND ANY EXPRESS OR      *
+# * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
+# * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO      *
+# * EVENT SHALL <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,        *
+# * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  *
+# * BUT NOT LIMITED TO, PROCUREMEN OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    *
+# * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY           *
+# * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING  *
+# * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS              *
+# * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                    *
+# ***********************************************************************************
 
 from distutils.core import setup
 from distutils.extension import Extension
@@ -25,7 +31,7 @@ ext = Extension('evtk.cevtk', ['src/cevtk.c'], include_dirs = [np.get_include()]
 
 setup(
     name = 'evtk',
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'Export data as VTK binary files',
     author = 'Paulo Herrera',
     author_email = 'pauloa.herrera@gmail.com',
