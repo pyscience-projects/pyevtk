f428374183c0523a5ac43db6dcf264400c3b4afc
MuellerSeb <mueller.seb@posteo.de>
2020-04-22 22:34:18 +0200

    evtk: reformat with black

--- evtk/__init__.py
+++ evtk/__init__.py
@@ -1,4 +1,8 @@
 from pyevtk import *
 
 import warnings
-warnings.warn('the "evtk" package is deprecated, use "pyevtk" instead', DeprecationWarning)
+
+warnings.warn(
+    'the "evtk" package is deprecated, use "pyevtk" instead',
+    DeprecationWarning,
+)
