a7e30e6d26bf79012b7dc0cc4ea3c8ac9efbe1a5
Sebastian Müller <mueller.seb@posteo.de>
2023-06-05 16:40:05 +0200

    remove py36 test, add py311 test

--- setup.py
+++ setup.py
@@ -57,9 +57,10 @@ setup(
     setup_requires=["pytest-runner"],
     tests_require=["pytest>=3.1", "pytest-cov", "twine", "check-manifest"],
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
